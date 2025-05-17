13f5c12260c89d4f33f34a7fb67a46f5b2e0c39b
Ten-T-Tonmoy <stu01754@gmail.com>
2025-05-17 23:16:44 +0600

    navbar innit mate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,14 @@ import { Routes, Route, Link } from "react-router-dom";
 import Landingpage from "./pages/landing/landingpage";
 import Aboutpage from "./pages/about/aboutpage";
 import Mainpage from "./pages/main/mainpage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="p-4 gap-4 flex flex-col">
-      
-
+    <div className=" gap-4 flex flex-col">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Mainpage/>}/>
+        <Route path="/" element={<Mainpage />} />
         <Route path="/about" element={<Aboutpage />} />
         <Route path="/home" element={<Landingpage />} />
       </Routes>
