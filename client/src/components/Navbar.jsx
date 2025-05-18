5c071cc998d31ed1be0bd1b98e03b1a4482fcdf8
Ten-T-Tonmoy <stu01754@gmail.com>
2025-05-18 23:34:33 +0600

    hate messing things up

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,56 @@
 import React from "react";
 //from daisy ui
-import logo2 from "../assets/logo2.png";
+import logo from "../assets/logo.png";
+import { FaBars, FaSearch } from "react-icons/fa";
+
+/**
+ * Home
+ * services
+ * about us
+ * ||
+ * contact redish
+ * login yellosh
+ */
+
+/**
+ * search box
+ *
+ * home
+ * services
+ * about us
+ * contact
+ * login
+ * icons
+ *
+ * start here yelloish
+ * login redish
+ */
 
 const Navbar = () => {
   return (
-    <div className="drawer">
+    <div className="drawer ">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
+      <div className="drawer-content  flex flex-col">
         {/* Navbar */}
-        <div className="navbar bg-base-300 w-full">
+        <div className="navbar bg-primary w-full ">
+          {/**fricking nav button */}
           <div className="flex-none lg:hidden">
             <label
               htmlFor="my-drawer-3"
               aria-label="open sidebar"
               className="btn btn-square btn-ghost"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-6 w-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
+              <FaBars className="scale-150" />
             </label>
           </div>
-          <div className="mx-2 flex-1 px-2">Navbar Title</div>
+
+          <img
+            src={logo}
+            className="
+            h-[60px]
+            "
+            alt=""
+          />
           <div className="hidden flex-none lg:block">
             <ul className="menu menu-horizontal">
               {/* Navbar menu content here */}
@@ -46,19 +66,65 @@ const Navbar = () => {
         {/* Page content here */}
         Content
       </div>
-      <div className="drawer-side">
+      <div className="drawer-side ">
         <label
           htmlFor="my-drawer-3"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+        <ul className="menu bg-violet-950 min-h-full w-[60vw] sm:w-64 items-center ">
           {/* Sidebar content here */}
           <li>
-            <a>Sidebar Item 1</a>
+            <div className="join mt-8 mb-2">
+              <label className="input text-black bg-white validator join-item">
+                <input
+                  type="email"
+                  placeholder="Search ..."
+                  className="text-[1.08rem] 
+                "
+                  required
+                />
+              </label>
+              <button className="btn btn-warning join-item">
+                <FaSearch className="scale-125  " />
+              </button>
+            </div>
+          </li>
+          <hr className="w-full bg-gray-500 " />
+
+          {/**search box goes here */}
+          <li>
+            <a>Home</a>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <a>Contact</a>
+          </li>
+          {/**Accordian shits */}
+          <li>
+            <div className="collapse p-0 ">
+              <input type="checkbox" className="peer " />
+              <div
+                className="collapse-title bg-violet-950 text-primary-content 
+              peer-checked:bg-secondary peer-checked:text-secondary-content text-center"
+              >
+                Services
+              </div>
+              <div
+                className="collapse-content bg-primary text-primary-content 
+              peer-checked:bg-secondary "
+              >
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
+              </div>
+            </div>
+          </li>
+          <li></li>
+          {/**Accordian shits */}
+          <li>
+            <button className="btn btn-warning w-[100%] ">Start Here</button>
+          </li>
+          <li>
+            <button className="btn btn-error ">Login</button>
           </li>
         </ul>
       </div>
